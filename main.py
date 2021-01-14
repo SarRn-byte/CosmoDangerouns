ea043e7a89f5bdd687d7c1f7a9f11514ddec12be
SarRn-byte <75025576+SarRn-byte@users.noreply.github.com>
2021-01-14 11:35:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,15 @@ import random
 WIDTH = 480
 HEIGHT = 600
 FPS = 60
+SPEED = 8
 
-# Задаем цвета
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
-SPEED = 8
 
-# Создаем игру и окно
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -53,27 +51,46 @@ class Player(pygame.sprite.Sprite):
             self.rect.top = 0
 
 
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 40))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(1, 8)
+        self.speedx = random.randrange(-3, 3)
+
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 8)
+
+
 all_sprites = pygame.sprite.Group()
+mobs = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
+for i in range(8):
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
 
+running = True
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-def main():
-    running = True
-    while running:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+    all_sprites.update()
 
-        all_sprites.update()
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    pygame.display.flip()
 
-        screen.fill(BLACK)
-        all_sprites.draw(screen)
-        pygame.display.flip()
-
-    pygame.quit()
-
-
-if __name__ == '__main__':
-    main()
+pygame.quit()
