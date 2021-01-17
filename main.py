5541dce5e8b47c20fec2cbc8c7bd08ac9f5e7072
Kostya-0 <62062644+Kostya-0@users.noreply.github.com>
2021-01-17 14:43:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ class Mob(pygame.sprite.Sprite):
         if now - self.last_update > 50:
             self.last_update = now
             self.rot = (self.rot + self.rot_speed) % 360
-            new_image = pygame.transform.rotate(self.image_orig, self.rot)
+            new_image = pygame.transform.rotate(self.image_orig, self.rot).convert_alpha()
             old_center = self.rect.center
             self.image = new_image
             self.rect = self.image.get_rect()
