9265d43b41f9e6a15b16b97c1bccf9e017f98f64
Kostya-0 <62062644+Kostya-0@users.noreply.github.com>
2021-01-17 14:21:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -260,7 +260,7 @@ class Explosion(pygame.sprite.Sprite):
 # Р—Р°РіСЂСѓР·РєР° РІСЃРµР№ РёРіСЂРѕРІРѕР№ РіСЂР°С„РёРєРё
 background = pygame.image.load(path.join(img_dir, "starfield.png")).convert()
 background_rect = background.get_rect()
-player_img = pygame.image.load(path.join(img_dir, "playerShip1_orange.png")).convert_alpfa()
+player_img = pygame.image.load(path.join(img_dir, "playerShip1_orange.png")).convert_alpha()
 player_mini_img = pygame.transform.scale(player_img, (25, 19))
 player_mini_img.set_colorkey(BLACK)
 bullet_img = pygame.image.load(path.join(img_dir, "laserRed16.png")).convert()
